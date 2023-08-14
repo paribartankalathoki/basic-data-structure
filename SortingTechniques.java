491cb50571ffa9c93ff6af14c0feba4830407cb0
Paribartan Kalathoki <paribartan.kalathoki@sbsolutionsnepal.com>
2023-08-14 21:00:12 +0545

    added different sorting techniques

---
+++ SortingTechniques.java
@@ -0,0 +1,217 @@
+/**
+ * @author:- Paribartan Kalathoki
+ * @created on:- 14 Aug, 2023 at 08:52 PM
+ */
+
+import java.util.Arrays;
+
+public class SortingTechniques {
+
+    // Insertion Sort
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    // Quick Sort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    // Merge Sort
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = arr[m + 1 + j];
+        }
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Heap Sort
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i >= 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    /**
+     * Description:
+     * To heapify a subtree rooted with node i which is
+     * an index in arr[]. n is size of heap
+     *
+     * @param arr
+     * @param n
+     * @param i
+     */
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int left = 2 * i + 1; // left = 2*i + 1
+        int right = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        // If right child is larger than largest so far
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        // Insertion Sort
+        insertionSort(arr);
+        System.out.println("Insertion Sort: " + Arrays.toString(arr));
+
+        // Selection Sort
+        int[] arr2 = {64, 34, 25, 12, 22, 11, 90};
+        selectionSort(arr2);
+        System.out.println("Selection Sort: " + Arrays.toString(arr2));
+
+        // Bubble Sort
+        int[] arr3 = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr3);
+        System.out.println("Bubble Sort: " + Arrays.toString(arr3));
+
+        // Quick Sort
+        int[] arr4 = {64, 34, 25, 12, 22, 11, 90};
+        quickSort(arr4, 0, arr4.length - 1);
+        System.out.println("Quick Sort: " + Arrays.toString(arr4));
+
+        // Merge Sort
+        int[] arr5 = {64, 34, 25, 12, 22, 11, 90};
+        mergeSort(arr5, 0, arr5.length - 1);
+        System.out.println("Merge Sort: " + Arrays.toString(arr5));
+
+        // Heap Sort
+        int[] arr6 = {64, 34, 25, 12, 22, 11, 90};
+        heapSort(arr6);
+        System.out.println("Heap Sort: " + Arrays.toString(arr6));
+    }
+}
