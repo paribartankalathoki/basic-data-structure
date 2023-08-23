803eb28550eeb9e771c794dde56cc27711eb29ad
Paribartan Kalathoki <paribartan.kalathoki@sbsolutionsnepal.com>
2023-08-23 22:31:19 +0545

    added different sorting techniques

--- SortingTechniques.java
+++ SortingTechniques.java
@@ -182,6 +182,26 @@ public class SortingTechniques {
         }
     }
 
+    // Exchange Sort
+    public static void exchangeSort(int[] num, int size) {
+        int i, j, temp;
+        for (i = 0; i < size - 1; i++) {
+            // Outer Loop
+            for (j = i + 1; j < size; j++) {
+                // Inner Loop
+                // Sorting into ascending order if previous
+                // element bigger than next element we swap
+                // to make it in ascending order
+                if (num[i] > num[j]) {
+                    // Swapping
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
 
@@ -212,6 +232,11 @@ public class SortingTechniques {
         // Heap Sort
         int[] arr6 = {64, 34, 25, 12, 22, 11, 90};
         heapSort(arr6);
+        System.out.println("Heap Sort: " + Arrays.toString(arr6));
+
+        // Exchange Sort
+        int[] arr7 = {64, 34, 25, 12, 22, 11, 90};
+        exchangeSort(arr7, arr7.length);
         System.out.println("Heap Sort: " + Arrays.toString(arr6));
     }
 }
